1efa28f27abbb63a5c7082850a5fbc2cbc847bb3
heistester <hetester@126.com>
2022-05-03 22:21:54 +0800

    hetester 3

--- auth_login/views.py
+++ auth_login/views.py
@@ -4,9 +4,40 @@ from django.shortcuts import render, redirect
 # Create your views here.
 from django.urls import reverse
 
-
 def index(request):
     return render(request,'index.html')
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def page_not_found(request,expection):
     return render(request,'404.html')
+handler404='auth_login.views.page_not_found'
