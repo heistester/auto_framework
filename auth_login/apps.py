d7de34dc127980519951370be92660523f710fab
heistester <hetester@126.com>
2022-05-03 15:18:38 +0800

    hetester 3

---
+++ auth_login/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AuthLoginConfig(AppConfig):
+    name = 'auth_login'
---
+++ auth_login/models.py
@@ -0,0 +1,6 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Create your models here.
+class UserInfo(AbstractUser):
+    create_time=models.DateTimeField(auto_created=True)
